87764cdd902a520fc4a58be8b825a2cb7b25b897
wen <123@212.com>
2020-12-10 19:29:32 +0800

    修复数据3

--- src/test/java/com/controller/HelloController.java
+++ src/test/java/com/controller/HelloController.java
@@ -4,5 +4,6 @@ public class HelloController {
     public void hello(){
         System.out.println("更新数据1");
         System.out.println("更新数据2");
+        System.out.println("hot_fix修复数据3");
     }
 }
