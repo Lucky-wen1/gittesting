51a3dc4d970f34598d3576f0cd83ec949d98e22d
wen <123@212.com>
2020-12-10 19:13:49 +0800

    新建测试git连接项目

---
+++ src/main/java/com/learn/GittestingApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestingApplication.class, args);
+    }
+
+}
